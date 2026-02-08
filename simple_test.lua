ab7576b7f7f3b1329e4baa16dd683a17e37407fd
Chainix <trapison06@gmail.com>
2026-02-08 00:11:36 -0500

    Create simple_test.lua

---
+++ simple_test.lua
@@ -0,0 +1,14 @@
+-- Simple test script
+print("GITHUB LOADING WORKS!")
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "SUCCESS!";
+    Text = "GitHub script loaded! ✅";
+    Duration = 5;
+})
+```
+
+5. **Click "Commit changes"**
+
+6. **Then in CHAINIX, change TEST to use:**
+```
+https://raw.githubusercontent.com/ChainixScripts/chainix-scripts/main/simple_test.lua
